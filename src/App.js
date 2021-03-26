3bd8db99e9825f2941cd7606cd3d4faa50997e34
Fabian Rhoda <fabian.rhoda@talvaro.com>
2021-03-26 16:42:44 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          To mops Edit <code>src/App.js</code> and save to rofl.
         </p>
         <a
           className="App-link"
